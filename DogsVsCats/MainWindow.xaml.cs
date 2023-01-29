bcc07f7509cffffea75a9a29b5932619c87a51be
soeatu <soeatu@gmail.com>
2023-01-29 17:17:09 +0900

    プロジェクト ファイルを追加します。

---
+++ DogsVsCats/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DogsVsCats
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            imgResult.Source = null;
+            TrimScreen trimScreen = new TrimScreen();
+            trimScreen.ShowDialog();//スクリーンショット処理開始
+            _esc_flg = trimScreen._esc_flg;
+            //Escキーフラグ検知でQRReaderの処理を行わない
+            if (!_esc_flg)
+            {
+                ImageShow();
+
+            }
+            else
+            {
+                txtReadData.Text = "";
+            }
+        }
+    }
+}
---
+++ DogsVsCats/TrimScreen.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DogsVsCats
+{
+    /// <summary>
+    /// TrimScreen.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TrimScreen : Window
+    {
+        private Point _position;
+        private bool _trimEnable = false;
+        public bool _esc_flg = false;
+
+        public TrimScreen()
+        {
+            InitializeComponent();
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //　マルチモニターの全領域のサイズを取得
+            this.Top = SystemParameters.VirtualScreenTop;
+            this.Left = SystemParameters.VirtualScreenLeft;
+            this.Width = SystemParameters.VirtualScreenWidth;
+            this.Height = SystemParameters.VirtualScreenHeight;
+
+            // ジオメトリサイズの設定
+            this.ScreenArea.Geometry1 = new RectangleGeometry(new Rect(0.0, 0.0, this.Width, this.Height));        
+        }
+        /// <summary>
+        /// Escキーでキャプチャ処理終了イベントメソッド
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                //EscキーフラグをTure
+                _esc_flg = true;
+                Close();
+            }
+        }
+        private void Hand_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var path = sender as Path;
+            if (path == null)
+                return;
+
+            // 開始座標を取得
+            var point = e.GetPosition(path);
+            _position = point;
+
+            // マウスキャプチャの設定
+            _trimEnable = true;
+            this.Cursor = Cursors.Cross;
+            path.CaptureMouse();
+        }
+
+        private void Hand_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var path = sender as Path;
+            if (path == null)
+                return;
+
+            // 現在座標を取得
+            var point = e.GetPosition(path);
+
+            // マウスキャプチャの終了
+            _trimEnable = false;
+            this.Cursor = Cursors.Arrow;
+            path.ReleaseMouseCapture();
+
+            // 画面キャプチャ
+            CaptureScreen(point);
+
+            // アプリケーションの終了
+            this.Close();
+        }
+
+
+        /// <summary>
+        /// 指定されている画面領域を描写
+        /// </summary>
+        /// <param name="point"></param>
+        private void DrawStroke(Point point)
+        {
+            // 矩形の描画
+            var x = _position.X < point.X ? _position.X : point.X;
+            var y = _position.Y < point.Y ? _position.Y : point.Y;
+            var width = Math.Abs(point.X - _position.X);
+            var height = Math.Abs(point.Y - _position.Y);
+            this.ScreenArea.Geometry2 = new RectangleGeometry(new Rect(x, y, width, height));
+        }
+
+        /// <summary>
+        /// 実際に範囲指定された領域をキャプチャ　マウスの左ボタンを放されたときに実行
+        /// </summary>
+        /// <param name="point"></param>
+        public void CaptureScreen(Point point)
+        {
+            // 座標変換
+            var start = PointToScreen(_position);
+            var end = PointToScreen(point);
+
+            // キャプチャエリアの取得
+            var x = start.X < end.X ? (int)start.X : (int)end.X;
+            var y = start.Y < end.Y ? (int)start.Y : (int)end.Y;
+            var width = (int)Math.Abs(end.X - start.X);
+            var height = (int)Math.Abs(end.Y - start.Y);
+            if (width == 0 || height == 0)
+            {
+                return;
+            }
+
+            // スクリーンイメージの取得
+            using (var bmp = new System.Drawing.Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb))
+            using (var graph = System.Drawing.Graphics.FromImage(bmp))
+            {
+                // 画面をコピーする
+                graph.CopyFromScreen(new System.Drawing.Point(x, y), new System.Drawing.Point(), bmp.Size);
+
+                // イメージの保存
+                string folder = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                // string folder = "C:\\Users\\2227000\\Desktop\\C#\\Memo\\image\\";
+                try
+                {
+                    bmp.Save(System.IO.Path.ChangeExtension(System.IO.Path.Combine(folder, "QRimage"), "png"), System.Drawing.Imaging.ImageFormat.Png);
+                    bmp.Dispose();
+                }
+                catch (System.Runtime.InteropServices.ExternalException)
+                {
+                    MessageBox.Show("上手くスクリーンショットが行えませんでした。\nもう一度お試し下さい。", "警告", MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                }
+            }
+        }
+    }
+}
